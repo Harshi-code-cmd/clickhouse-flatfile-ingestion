609786ac2ab1c444aba202e364252dcc2aad2ae8
Harshi-code-cmd <harshib233@gmail.com>
2025-04-15 18:21:35 +0530

    Update config.py

--- Backend/config.py
+++ Backend/config.py
@@ -1,1 +1,9 @@
+class Config:
+    UPLOAD_FOLDER = "../data/uploads/"
+    OUTPUT_FOLDER = "../data/output/"
+    ALLOWED_EXTENSIONS = {"csv"}
+
+    @staticmethod
+    def allowed_file(filename):
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in Config.ALLOWED_EXTENSIONS
 
