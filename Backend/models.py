7f95eb13a1947d6768bffebfd2bd72b40a757381
Harshi-code-cmd <harshib233@gmail.com>
2025-04-15 18:22:16 +0530

    Create models.py

---
+++ Backend/models.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class ClickHouseConfig(BaseModel):
+    host: str
+    port: int
+    database: str
+    user: str
+    jwt_token: str
+
+class IngestionRequest(BaseModel):
+    source_type: str
+    target_type: str
+    clickhouse_config: ClickHouseConfig
+    flatfile_name: str
+    columns: list
