56689e2709326743fae1509629dba75e0a2bd00c
Harshi-code-cmd <harshib233@gmail.com>
2025-04-15 12:37:28 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,1 +1,16 @@
+from pydantic import BaseModel
+
+class ClickHouseConfig(BaseModel):
+    host: str
+    port: int
+    database: str
+    user: str
+    jwt_token: str
+
+class IngestionRequest(BaseModel):
+    source_type: str
+    target_type: str
+    clickhouse_config: ClickHouseConfig
+    flatfile_name: str
+    columns: list
 
